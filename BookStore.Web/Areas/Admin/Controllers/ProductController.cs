65940cf2a3b413daffb48a8f74808a4550e1b2b0
vassdeniss <vassdeniss@gmail.com>
2023-05-16 11:00:09 +0300

    Ger products

---
+++ BookStore.Web/Areas/Admin/Controllers/ProductController.cs
@@ -0,0 +1,158 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+using BookStore.Infrastructure.Models;
+using BookStore.Infrastructure.Repository.Contracts;
+using BookStore.Web.ViewModels;
+
+using Microsoft.AspNetCore.Mvc;
+
+namespace BookStore.Web.Areas.Admin.Controllers
+{
+    public class ProductController : BaseAdminController
+    {
+        private readonly IUnitOfWork unitOfWork;
+
+        public ProductController(IUnitOfWork unitOfWork)
+        {
+            this.unitOfWork = unitOfWork;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> IndexAsync()
+        {
+            IEnumerable<Product> dbProducts = await this.unitOfWork.ProductRepository.GetAllAsync();
+
+            IEnumerable<ProductViewModel> products = dbProducts.Select((product) => new ProductViewModel
+            {
+                Id = product.Id,
+                Title = product.Title,
+                Description = product.Description,
+                ISBN = product.ISBN,
+                Author = product.Author,
+                ListPrice = product.ListPrice,
+                Price = product.Price,
+                Price50 = product.Price50,
+                Price100 = product.Price100,
+            });
+
+            return this.View(products);
+        }
+
+        [HttpGet]
+        public IActionResult Create()
+        {
+            return this.View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> CreateAsync(CategoryViewModel category)
+        {
+            if (!this.ModelState.IsValid)
+            {
+                return this.View();
+            }
+
+            Category dbCategory = new Category()
+            {
+                Name = category.Name,
+                DisplayOrder = category.DisplayOrder,
+            };
+
+            await this.unitOfWork.CategoryRepository.AddAsync(dbCategory);
+            await this.unitOfWork.SaveAsync();
+
+            this.TempData["SuccessMessage"] = "Category created successfully!";
+            return this.RedirectToAction("Index");
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Edit(Guid? id)
+        {
+            if (id is null)
+            {
+                return this.NotFound();
+            }
+
+            Category? category = await this.unitOfWork.CategoryRepository.GetByIdAsync(id);
+            if (category is null)
+            {
+                return this.NotFound();
+            }
+
+            CategoryViewModel categoryViewModel = new CategoryViewModel
+            {
+                Id = category.Id,
+                Name = category.Name,
+                DisplayOrder = category.DisplayOrder,
+            };
+
+            return this.View(categoryViewModel);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Edit(CategoryViewModel category)
+        {
+            if (!this.ModelState.IsValid)
+            {
+                return this.View();
+            }
+
+            Category dbCategory = (await this.unitOfWork.CategoryRepository.GetByIdAsync(category.Id))!;
+
+            dbCategory.Name = category.Name;
+            dbCategory.DisplayOrder = category.DisplayOrder;
+
+            this.unitOfWork.CategoryRepository.Update(dbCategory);
+
+            await this.unitOfWork.SaveAsync();
+
+            this.TempData["SuccessMessage"] = "Category edited successfully!";
+            return this.RedirectToAction("Index");
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> DeleteAsync(Guid? id)
+        {
+            if (id is null)
+            {
+                return this.NotFound();
+            }
+
+            Category? category = await this.unitOfWork.CategoryRepository.GetByIdAsync(id);
+            if (category is null)
+            {
+                return this.NotFound();
+            }
+
+            CategoryViewModel categoryViewModel = new CategoryViewModel
+            {
+                Id = category.Id,
+                Name = category.Name,
+                DisplayOrder = category.DisplayOrder,
+            };
+
+            return this.View(categoryViewModel);
+        }
+
+        [HttpPost]
+        [ActionName("Delete")]
+        public async Task<IActionResult> DeletePostAsync(Guid? id)
+        {
+            Category? category = await this.unitOfWork.CategoryRepository.GetByIdAsync(id!);
+            if (category is null)
+            {
+                return this.NotFound();
+            }
+
+            this.unitOfWork.CategoryRepository.Remove(category);
+            await this.unitOfWork.SaveAsync();
+
+            this.TempData["SuccessMessage"] = "Category deleted successfully!";
+            return this.RedirectToAction("Index");
+        }
+    }
+}
---
+++ BookStore.Web/ViewModels/ProductViewModel.cs
@@ -0,0 +1,44 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System;
+
+namespace BookStore.Web.ViewModels
+{
+    public class ProductViewModel
+    {
+        public Guid Id { get; set; }
+
+        [Required]
+        public string Title { get; set; } = null!;
+
+        [StringLength(1000, MinimumLength = 1, ErrorMessage = "{0} must have a length of {2}-{1}.")]
+        public string? Description { get; set; }
+
+        [Required]
+        [StringLength(26, MinimumLength = 13, ErrorMessage = "{0} must have a length of {2}-{1}.")]
+        public string ISBN { get; set; } = null!;
+
+        [Required]
+        [StringLength(60, ErrorMessage = "{0} cannot be longer than {1}.")]
+        public string Author { get; set; } = null!;
+
+        [Required]
+        [Display(Name = "List Price")]
+        [Range(1, 1000)]
+        public double ListPrice { get; set; }
+
+        [Required]
+        [Display(Name = "Price for 1-50")]
+        [Range(1, 1000)]
+        public double Price { get; set; }
+
+        [Required]
+        [Display(Name = "Price for 50+")]
+        [Range(1, 1000)]
+        public double Price50 { get; set; }
+
+        [Required]
+        [Display(Name = "Price for 100+")]
+        [Range(1, 1000)]
+        public double Price100 { get; set; }
+    }
+}
